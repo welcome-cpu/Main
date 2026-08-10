ad9eb16cadc643b16a3e02258bb5b31fe0ab9947
Shaun Donaldson <welcome@gamriechalets.co.uk>
2026-08-10 22:06:04 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">
+        Hello from Claude Code
+      </h1>
+    </div>
+  );
+}
